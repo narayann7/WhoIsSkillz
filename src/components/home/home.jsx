6261715be822f2dae78add34b1b42c9e73785e5d
narayann7 <laxminarayanreddy432@gmail.com>
2022-04-06 00:39:43 +0700

    added-animations

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -7,12 +7,20 @@ function home() {
         <div className="center_container just-center-c">
           <h4>Hello 👋 , I’m</h4>
           <h1>Laxmi Narayan</h1>
-          <h3>App developer .</h3>
+          <div class="imWrapper">
+            <div class="words just-center-c ">
+              <span><h3>App developer .</h3> </span>
+              <span><h3>Backend enthusiastic .</h3> </span>
+              <span><h3>Problem Slover . </h3> </span>
+
+
+
+     
+            </div>
+          </div>
         </div>
 
-        <div className="test">
-
-        </div>
+        <div className="test"></div>
       </div>
     </section>
   );
