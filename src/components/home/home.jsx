070a348c65cab96d23464e059ecc09adf3e26fba
narayann7 <laxminarayanreddy432@gmail.com>
2022-04-06 22:33:43 +0700

    animation-done-added-icons

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import "./home.css";
+import google from "../../assets/icons/Google_bw.png";
 function home() {
   return (
     <section className="main_section  just-center-r">
-      <div className="main_container">
+      <div className="main_container just-center-c">
         <div className="center_container just-center-c">
           <h4>Hello 👋 , I’m</h4>
           <h1>Laxmi Narayan</h1>
           <div class="imWrapper">
             <div class="words just-center-c ">
-              <span><h3>App developer .</h3> </span>
-              <span><h3>Backend enthusiastic .</h3> </span>
-              <span><h3>Problem Slover . </h3> </span>
-
-
-
-     
+              <span>
+                <h3>App developer .</h3>{" "}
+              </span>
+              <span>
+                <h3>Backend enthusiastic .</h3>{" "}
+              </span>
+              <span>
+                <h3>Problem Slover . </h3>{" "}
+              </span>
+              <span>
+                <h3>Backend enthusiastic .</h3>{" "}
+              </span>
+              <span>
+                <h3>App developer .</h3>{" "}
+              </span>
             </div>
           </div>
         </div>
-
-        <div className="test"></div>
       </div>
+
+
+ 
+      
+      <div class="socials just-center-c">
+          <div className="linkedin"></div>
+
+          <div className="github"></div>
+
+          <div className="gmail"></div>
+        </div>
     </section>
   );
 }
