0c4833ac8b309bb8418f3bfe79379c0b2ea7f871
narayann7 <laxminarayanreddy432@gmail.com>
2022-04-11 22:35:45 +0700

    added-about-section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,17 +1,27 @@
 import "./about.css";
+import {
+  spotify_link,
+  instagram_link,
+  twitter_link,
+} from "../../utility/links.js";
+import Lx_dp from "../../assets/images/lx_dp.png";
 const About = () => {
-  return <section id="about" class="main_about_section just-center-c">
+  return (
+    <section id="about" class="main_about_section just-center-c">
+      <section className="about_container just-center-r">
+        <div className="about_left">
+          <img src={Lx_dp} alt="narayan" />
 
-<section className="about_container">
-
-
-
-
-</section>
-
-
-
-  </section>;
+          <div class="about_socials just-center-c">
+            <a href={twitter_link} target="_blank" className="twitter"></a>
+            <a href={instagram_link} target="_blank" className="instagram"></a>
+            <a href={spotify_link} target="_blank" className="spotify"></a>
+          </div>
+        </div>
+        <div className="about_right"></div>
+      </section>
+    </section>
+  );
 };
 
 export default About;
--- src/utility/links.js
+++ src/utility/links.js
@@ -3,12 +3,17 @@ const resume_link = "https://drive.google.com/file/d/1Na3eF-zfvtdzZ4kxkIXDIbqOyf
 const linkedin_link = "https://www.linkedin.com/in/narayann/"
 const github_link = "https://github.com/narayann7"
 const gmail_link = "mailto:laxminarayanreddy432@gmail.com"
-
+const twitter_link = "https://twitter.com/_narayann"
+const instagram_link = "https://www.instagram.com/_narayann/"
+const spotify_link = "https://open.spotify.com/user/jkqb4becefsfolv4ynnd7hrrq?si=mGdUR3UCS2emOlu0Y9v_3A&utm_source=copy-link"
 module.exports = {
     resume_link: resume_link
     , linkedin_link: linkedin_link,
     github_link: github_link,
-    gmail_link: gmail_link
+    gmail_link: gmail_link,
+    twitter_link: twitter_link,
+    instagram_link: instagram_link,
+    spotify_link: spotify_link
 
 }
 
